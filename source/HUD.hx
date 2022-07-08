a47c7d3b28f7025728fabf2d80e7ca25bf02fada
oblanmaN <50035022+oblanmaN@users.noreply.github.com>
2022-07-07 21:54:35 -0700

    modified:   source/HUD.hx

--- source/HUD.hx
+++ source/HUD.hx
@@ -27,5 +27,7 @@ class HUD extends FlxTypedGroup<FlxSprite>
 		background.drawRect(0, 19, FlxG.width, 1, FlxColor.WHITE);
 		healthCounter = new FlxText(16, 2, 0, "100", 8);
 		healthCounter.setBorderStyle(SHADOW, FlxColor.GRAY, 1, 1);
+		manaCounter = new FlxText(0, 2, 0, "100", 8);
+		manaCounter.setBorderStyle(SHADOW, FlxColor.GRAY, 1, 1);
 	}
 }
